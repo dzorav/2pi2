04d7417c1d4740797d4a404d26352d75333baea8
Abhinav Joshi <abhinavjoshi.aj86@gmail.com>
2021-06-17 20:50:27 +0530

    dsfdsf

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var static = require('node-static');
 var http = require('http');
-
+var port = process.env.PORT || 80
 var file = new (static.Server)(__dirname);
 
 http.createServer(function (req, res) {
     file.serve(req, res);
-}).listen();
+}).listen(port);
